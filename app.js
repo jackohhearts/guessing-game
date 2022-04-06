01048e83c76ca43c67be4f887dd46b591721d59b
Joseph Campbell <thecanadianjoe@gmail.com>
2022-04-06 16:22:56 -0400

    added instructions, css, and quit feature

--- app.js
+++ app.js
@@ -10,14 +10,21 @@ const targetNum = Math.floor(Math.random() * maximum) + 1;
 console.log(targetNum)
 
 let guess = parseInt(prompt("What is your guess?"));
-while (guess !== targetNum) {
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum) {
+    if (guess === 'quit') break;
+    attempts++;
     if (guess > targetNum) {
-        guess = parseInt(prompt("Sorry, too high! Enter a new number:"))
+        guess = prompt("Sorry, too high! Enter a new number:")
     } else {
-        guess = parseInt(prompt("Sorry, too low! Enter a new number:"))
+        guess = prompt("Sorry, too low! Enter a new number:")
     }
 }
-if (guess == targetNum) {
-    console.log("You Win!")
+if (guess === 'quit') {
+    console.log("You quit!  That's okay.")
+} else {
+    console.log(`You Win! It took you ${attempts} guesses!`)
 }
+
 
